e454ee9b2311f43878ea3e7675928d760305aa1c
Robson Rodrigues Junior <robson.rodrigues477557@outlook.com>
2024-02-28 23:22:32 -0300

    Adicionado primeng ao projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { MenubarModule } from 'primeng/menubar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule, MenubarModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'perfil';
+export class AppComponent implements OnInit {
+  items: MenuItem[] | undefined;
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Home',
+        icon: 'pi pi-fw pi-file',
+      },
+      {
+        label: 'Projetos',
+        icon: 'pi pi-fw pi-pencil',
+        items: [
+          {
+            label: 'Perfil',
+            icon: 'pi pi-fw pi-align-left',
+          },
+          {
+            label: 'Registro de vendas',
+            icon: 'pi pi-fw pi-align-right',
+          },
+          {
+            label: 'API de CEP quarkus',
+            icon: 'pi pi-fw pi-align-center',
+          },
+        ],
+      },
+    ];
+  }
 }
